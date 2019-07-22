e7028fa861f03dc6d2e062bfa78b1181d69e13a1
Bryan <bryanyap972@gmail.com>
2019-07-22 17:48:10 +0800

    Features: Prolfile, Login, Users, Chat, Requests

--- app/src/main/java/com/example/orbital/UsersFragment.java
+++ app/src/main/java/com/example/orbital/UsersFragment.java
@@ -161,6 +161,8 @@ public class UsersFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_main, menu);
 
+        menu.findItem(R.id.action_add_post).setVisible(false);
+
         MenuItem item = menu.findItem(R.id.action_search);
         final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
 
